56f7e57f65df3086d82342493d811fe6c799a578
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-03-08 16:57:49 -0300

    change to default widgets

--- lib/chat_page/chat_page.dart
+++ lib/chat_page/chat_page.dart
@@ -1,8 +1,5 @@
-import 'package:chat_bot_interaction/chat_page/widgets/bot_message_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_composer/flutter_chat_composer.dart';
-import 'package:flutter_chat_composer/widgets/bot_user_open_text.dart';
-import 'package:chat_bot_interaction/chat_page/widgets/user_message_widget.dart';
 import 'my_chat_bot.dart';
 
 class ChatPage extends StatelessWidget {
@@ -17,10 +14,7 @@ class ChatPage extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: ChatBotWidget(
         chatBot: chatBot,
-        botMessageWidget: (message) => BotMessageWidget(message: message),
         botTransitionWidget: (message) => _botTransitionWidget(message),
-        userMessageWidget: (message) => UserMessageWidget(message: message),
-        userOpenTextWidget: _userOpenTextWidget(chatBot),
         sameUserSpacing: 3,
       ),
     );
@@ -28,29 +22,5 @@ class ChatPage extends StatelessWidget {
 
   Widget _botTransitionWidget(RichText message) {
     return Container();
-  }
-
-  BotUserOpenText _userOpenTextWidget(ChatBot chatBot) {
-    TextEditingController controller = TextEditingController();
-    return BotUserOpenText(
-      chatBot: chatBot,
-      controller: controller,
-      textField: Row(
-        children: [
-          Flexible(child: Container()),
-          Flexible(
-            child: TextField(
-              controller: controller,
-            ),
-          ),
-        ],
-      ),
-      userMessageWidget: (message) => UserMessageWidget(message: message),
-      icon: Icon(
-        Icons.send,
-        color: Colors.grey[500],
-        size: 18,
-      ),
-    );
   }
 }
